0579f30c02471c352a0c733a7ba6f8e8d13e7b90
Tom Dirinck <tom.dirinck@gmail.com>
2023-10-05 14:55:04 +0200

    add workspaces

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,20 +4,36 @@ import {visionTool} from '@sanity/vision'
 //import {googleMapsInput} from '@sanity/google-maps-input'
 import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Nyzalia',
+export default defineConfig([
+  {
+    name: 'production-workspace',
+    title: 'Nyzalia',
 
-  projectId: 'mxnfue20',
-  dataset: 'develop',
+    projectId: 'mxnfue20',
+    dataset: 'production',
+    basePath: '/production',
 
-  plugins: [
-    deskTool(),
-    visionTool(),
-    //googleMapsInput(),
-  ],
-
-  schema: {
-    types: schemaTypes,
+    plugins: [deskTool(), visionTool()],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'develop-workspace',
+    title: 'Nyzalia-develop',
+
+    projectId: 'mxnfue20',
+    dataset: 'develop',
+    basePath: '/develop',
+
+    plugins: [
+      deskTool(),
+      visionTool(),
+      //googleMapsInput(),
+    ],
+
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
